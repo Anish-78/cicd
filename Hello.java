4eb324569dab0ca51c47d692f0092e1dda225cb2
Anish Malang <anishmalang1782@gmail.com>
2023-10-02 13:37:48 +0530

    .....

---
+++ Hello.java
@@ -0,0 +1,16 @@
+/**
+ * Hello
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        
+    }
+    public String exmpl1() {
+        return "Hello";
+    }
+
+    public String exmpl2() {
+        return "Hello";
+    }
+}
